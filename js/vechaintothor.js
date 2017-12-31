e0c76416b81f7dbbaead75db08ca86b351ae1294
Jeremy <jeremy.s.sutton@gmail.com>
2017-12-31 14:05:10 -0500

    fix js bugs for thor calc

--- js/vechaintothor.js
+++ js/vechaintothor.js
@@ -5,21 +5,21 @@
 	var thorDollarPrice = 5;
 	var isAuthorityNode = false;
 	var isMjolnirNode = false;
-	var isThunderNode = false;
+	var isThunderNode = true;
 	var isStrengthNode = false;
 
 	var calculateThor = function(vet, A, M, T, S) {
 		var bonus = (A || M) ? 2 : (T ? 1.5 : (S ? 1 : 0));
-		var thorPerDay = B + (NB * bonus);
+		var thorPerDay = (B + (NB * bonus)) * vet;
 		var thorPerYear = thorPerDay * 365;
 		var dollarPerYear = thorPerYear * thorDollarPrice;
 
 		return "You will generate " + thorPerDay + " Thor per day, " + thorPerYear +
-			" Thor per year, which equals $" + dollarPerYear " per year at $5 per Thor";
+			" Thor per year, which equals $" + dollarPerYear + " per year at $5 per Thor";
 	};
 
 	$(calculateButton).on("click", function() {
-		var vetAmount = $("vetamount").value();
+		var vetAmount = $("#vetamount").val();
 		if($.isNumeric(vetAmount)) {
 			var thorAmount = calculateThor(vetAmount, isAuthorityNode, isMjolnirNode, isThunderNode, isStrengthNode);
 			alert(thorAmount);
